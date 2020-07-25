60828d69ce6d53c9d659f13aab3c2983e283b82e
Administration <gesoftnet@gmail.com>
2020-07-25 14:01:33 +0800

    testvue

--- src/main.js
+++ src/main.js
@@ -4,24 +4,32 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import axios from  'axios/lib/axios'
+import axios from 'axios/lib/axios'
 import './assets/css/global.css'
+import "@ckeditor/ckeditor5-build-classic/build/translations/zh-cn";
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import CKEditor from '@ckeditor/ckeditor5-vue'
 // 引入样式
 import 'vue-easytable/libs/themes-base/index.css'
 // 导入 table 和 分页组件
-import {VTable,VPagination} from 'vue-easytable'
+import {VTable, VPagination} from 'vue-easytable'
 import Mint from 'mint-ui'
 import 'mint-ui/lib/style.css'
+
+
 // 注册到全局
 Vue.component(VTable.name, VTable)
 Vue.component(VPagination.name, VPagination)
+Vue.component(ClassicEditor);
+Vue.component(CKEditor);
 
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
-Vue.use(Mint)
-Vue.use(ElementUI)
+Vue.use(Mint);
+Vue.use(ElementUI);
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
